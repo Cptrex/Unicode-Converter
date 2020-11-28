3888fdeef47e37c449f1bf1d5504a89d871e1c93
Cptrex <38324542+Cptrex@users.noreply.github.com>
2020-11-28 23:08:09 +0600

    Добавьте файлы проекта.

---
+++ Unicode Converter/Converter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using Unicode_Converter;
+
+namespace UnicodeConverter
+{
+    class Converter
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите название файла(с расширением): ");
+                string fileName = Console.ReadLine();
+                Console.Write("Введите путь к файлу: ");
+                string path = Console.ReadLine();
+            
+                FileInputData TargetFile = new FileInputData(fileName, path);
+                string readText = File.ReadAllText(TargetFile.InputPath);
+                Console.WriteLine(readText);
+                foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(TargetFile))
+                {
+                    string name = descriptor.Name;
+                    object value = descriptor.GetValue(TargetFile);
+                    Console.WriteLine($"{name} = {value}");
+                }
+            }
+            catch(Exception ex) { Console.WriteLine($"Что-то пошло не так! |{ex}|"); }
+        }
+    }
+}
---
+++ Unicode Converter/FileInputData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Unicode_Converter
+{
+    class FileInputData
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public string InputPath { get; set; }
+        public string OutputPath { get; set; }
+        public string Encoding { get; set; }
+        public long Size { get; set; } = 0; 
+        public string Extension { get; set; }
+       public FileInputData(string name, string inputPath)
+        {
+            FileInfo fi = new FileInfo(inputPath + @"\" + name);
+            Size = fi.Length;
+            Extension = fi.Extension;
+            FullName = fi.Name;
+            InputPath = fi.DirectoryName + @"\" + FullName;
+            Name = FullName.Replace(fi.Extension, "");
+            OutputPath = InputPath.Replace(FullName,"") + Name + "NEW" + fi.Extension;
+        }
+    }
+}
